b321aa5e6a259beda35749c8a67ec51775f223cc
vishal modanwal <vk0764394@gmail.com>
2026-04-07 15:05:41 +0530

    landing ,signup, login page fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes ,{
+      scrollPositionRestoration: 'top',   // 🔥 THIS LINE FIXES IT
+      anchorScrolling: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
