5ad44d14b2f8e3ac8156519dcbd39eaf7621a59a
vishal modanwal <vk0764394@gmail.com>
2026-04-08 15:29:12 +0530

    navbar pending

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,18 +2,34 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',   // 🔥 MUST be this
+  selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-    constructor(private router: Router) {}
-    isLoggedIn(): boolean {
-  return !!localStorage.getItem('token');
+
+  constructor(private router: Router) {}
+
+  allowedRoutes = ['/dashboard', '/budget'];
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+goToDashBoard(){
+  if(this.isLoggedIn() && this.allowedRoutes.includes("/dashboard")){
+    console.log("hiii")
+    this.router.navigate(['/dashboard']);
+  }else{
+    console.log("error");
+  }
+}
+showLogout(): boolean {
+  return this.isLoggedIn() && this.allowedRoutes.includes(this.router.url);
 }
 
-    logout() {
-      localStorage.removeItem('token');
-      this.router.navigate(['/login']);
-    }
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
